bb56dcbeb68b8a3e93da49c5d4877df903600000
Knika Dass <knikadass@gmail.com>
2020-07-22 03:16:20 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -18,3 +18,21 @@ class Dog
 
 
 end
+
+class Person
+
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+
+  def job=(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+end
